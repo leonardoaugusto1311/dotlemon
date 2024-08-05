3f2e6db50caec3e3e3312d0c0a31850daf04a12d
Price Survey Data <leonardoaugusto13@hotmail.com>
2024-08-05 15:58:05 -0300

    Update ⏩_5_Lançamentos.py

--- pages/⏩_5_Lançamentos.py
+++ pages/⏩_5_Lançamentos.py
@@ -91,7 +91,7 @@ if st.button("Enviar"):
        
        #st.write(percentuais_politicas)
        faixa3 = 0
-       st.write(fat_lancamento)
+       st.write(cliente_m)
        st.write(cliente_p)
        st.write(fat_lancamento-cliente_p)
        st.write(depesas_clientem-depesas_clientep)
