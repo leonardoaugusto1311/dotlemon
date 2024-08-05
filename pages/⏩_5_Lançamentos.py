f505b583717ab4cdc2fb076437210db30a13a0b0
Price Survey Data <leonardoaugusto13@hotmail.com>
2024-08-05 15:53:20 -0300

    Update ⏩_5_Lançamentos.py

--- pages/⏩_5_Lançamentos.py
+++ pages/⏩_5_Lançamentos.py
@@ -100,10 +100,10 @@ if st.button("Enviar"):
         st.write(f'Comissão Faixa 3 R$: {faixa3}')              
     
     umenosaliquota = 1-(aliquota_imposto/100)
-    comissao_recebida = faixa1+faixa2+faixa3
-    st.write(f'Recebido a título de comissão R$: {comissao_recebida}')
+    #comissao_recebida = faixa1+faixa2+faixa3
+    #st.write(f'Recebido a título de comissão R$: {comissao_recebida}')
 
-    tx_fix_mensal  = round(contribuicao_cliente/umenosaliquota,2)
-    st.write(f'Taxa Fixa Mensal: {tx_fix_mensal}')
-    total_recebido = tx_fix_mensal*projeto_meses+comissao_recebida
-    st.write(f'Total Recebido no Projeto: {total_recebido}')
+    #tx_fix_mensal  = round(contribuicao_cliente/umenosaliquota,2)
+    #st.write(f'Taxa Fixa Mensal: {tx_fix_mensal}')
+    #total_recebido = tx_fix_mensal*projeto_meses+comissao_recebida
+    #st.write(f'Total Recebido no Projeto: {total_recebido}')
