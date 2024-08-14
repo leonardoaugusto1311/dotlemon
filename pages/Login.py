402bf6c86a8a880156eca2776282251c1dfc480f
Price Survey Data <93108148+leonardoaugusto13@users.noreply.github.com>
2024-08-14 19:45:25 -0300

    Update Login.py

--- pages/Login.py
+++ pages/Login.py
@@ -3,31 +3,42 @@ import pandas as pd
 import requests
 from io import BytesIO
 
-# Função para baixar e carregar a planilha do OneDrive
 def load_data_from_onedrive():
     onedrive_link = "https://api.onedrive.com/v1.0/shares/s!AvctgKBRMfiLgpd9WC1dgiFxTfGrSA/root/content"
     response = requests.get(onedrive_link)
     response.raise_for_status()  # Verifica se o download foi bem-sucedido
+
+    # Verifique se o conteúdo é realmente um arquivo Excel
+    content_type = response.headers.get('Content-Type')
+    st.write(f"Content-Type: {content_type}")
+
     excel_data = BytesIO(response.content)
-    df = pd.read_excel(excel_data, engine='openpyxl')  # Certifique-se de especificar o motor correto
+    # Tente detectar o tipo de arquivo
+    st.write("Detectando tipo de arquivo...")
+    
+    try:
+        df = pd.read_excel(excel_data, engine='openpyxl')
+        st.write("Arquivo carregado com sucesso.")
+    except Exception as e:
+        st.error(f"Erro ao carregar o arquivo: {e}")
+        return None
+
     return df
 
-# Função para verificar as credenciais
 def validate_credentials(username, password, df):
-    # Verifica se o nome de usuário e senha fornecidos estão na planilha
-    user_row = df[(df['Username'] == username) & (df['Password'] == password)]
-    return not user_row.empty
+    if df is not None:
+        user_row = df[(df['Username'] == username) & (df['Password'] == password)]
+        return not user_row.empty
+    return False
 
-# Página de login
 def main():
     st.title("Login Page")
 
-    # Carregar dados da planilha
     st.write("Carregando dados da planilha...")
     df = load_data_from_onedrive()
-    st.write("Dados carregados.")
+    if df is not None:
+        st.write("Dados carregados.")
 
-    # Formulário de login
     with st.form(key='login_form'):
         username = st.text_input("Username")
         password = st.text_input("Password", type='password')
