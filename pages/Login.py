3dbb7e80fe4a77fbf8f5a66df560d7c312ba73b2
Price Survey Data <93108148+leonardoaugusto13@users.noreply.github.com>
2024-08-14 19:40:22 -0300

    Create Login.py

---
+++ pages/Login.py
@@ -0,0 +1,42 @@
+import streamlit as st
+import pandas as pd
+import requests
+from io import BytesIO
+
+# Função para baixar e carregar a planilha do OneDrive
+def load_data_from_onedrive():
+    onedrive_link = "https://api.onedrive.com/v1.0/shares/s!AvctgKBRMfiLgpd9WC1dgiFxTfGrSA/root/content"
+    response = requests.get(onedrive_link)
+    response.raise_for_status()  # Verifica se o download foi bem-sucedido
+    excel_data = BytesIO(response.content)
+    df = pd.read_excel(excel_data)
+    return df
+
+# Função para verificar as credenciais
+def validate_credentials(username, password, df):
+    user_row = df[(df['Username'] == username) & (df['Password'] == password)]
+    return not user_row.empty
+
+# Página de login
+def main():
+    st.title("Login Page")
+
+    # Carregar dados da planilha
+    st.write("Carregando dados da planilha...")
+    df = load_data_from_onedrive()
+    st.write("Dados carregados.")
+
+    # Formulário de login
+    with st.form(key='login_form'):
+        username = st.text_input("Username")
+        password = st.text_input("Password", type='password')
+        submit_button = st.form_submit_button("Login")
+
+        if submit_button:
+            if validate_credentials(username, password, df):
+                st.success("Login bem-sucedido!")
+            else:
+                st.error("Nome de usuário ou senha inválidos.")
+
+if __name__ == "__main__":
+    main()
