409a2a1e5b37be8cae845fb4a5963d6a8a09a545
Price Survey Data <93108148+leonardoaugusto13@users.noreply.github.com>
2024-08-14 20:52:55 -0300

    troca da ordem de telas

---
+++ pages/Inicio.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import base64
+#st.image(r"C:\Users\user\Desktop\Projetos\Dot_lemon\dotlemon logo.png", width=200)
+
+# Funções para as páginas
+
+st.set_page_config(page_title="Dot Lemon",page_icon="🍋")
+st.title('Primeiros Passos') # titulo
+st.info('🟡Leia o passo a passo com atenção🟡') # informativo
+
+#.\dotlemon\Scripts\activate
+
+# Salvar últimos inputs de dados
