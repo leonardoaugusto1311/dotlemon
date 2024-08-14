7efed27a43eb9b2056a178f4b822ea9c0117fb33
Price Survey Data <93108148+leonardoaugusto13@users.noreply.github.com>
2024-08-14 19:56:38 -0300

    Update Login.py

--- pages/Login.py
+++ pages/Login.py
@@ -1,54 +1,28 @@
 import streamlit as st
-import pandas as pd
-import requests
-from io import BytesIO
+from pymongo import MongoClient
 
-def load_data_from_onedrive():
-    onedrive_link = "https://api.onedrive.com/v1.0/shares/s!AvctgKBRMfiLgpd9WC1dgiFxTfGrSA/root/content"
-    response = requests.get(onedrive_link)
-    response.raise_for_status()  # Verifica se o download foi bem-sucedido
+# Configurar a conexão com o MongoDB
+client = MongoClient('mongodb://localhost:27017/')
+db = client.sua_base_de_dados
+colecao = db.sua_colecao
 
-    # Verifique se o conteúdo é realmente um arquivo Excel
-    content_type = response.headers.get('Content-Type')
-    st.write(f"Content-Type: {content_type}")
-
-    excel_data = BytesIO(response.content)
-    # Tente detectar o tipo de arquivo
-    st.write("Detectando tipo de arquivo...")
-    
-    try:
-        df = pd.read_excel(excel_data, engine='openpyxl')
-        st.write("Arquivo carregado com sucesso.")
-    except Exception as e:
-        st.error(f"Erro ao carregar o arquivo: {e}")
-        return None
-
-    return df
-
-def validate_credentials(username, password, df):
-    if df is not None:
-        user_row = df[(df['Username'] == username) & (df['Password'] == password)]
-        return not user_row.empty
+# Função para verificar credenciais
+def verificar_credenciais(login, senha):
+    usuario = colecao.find_one({"login": login})
+    if usuario:
+        senha_armazenada = usuario.get("senha")
+        return senha_armazenada == senha
     return False
 
-def main():
-    st.title("Login Page")
+# Criar a interface de login
+st.title("Página de Login")
 
-    st.write("Carregando dados da planilha...")
-    df = load_data_from_onedrive()
-    if df is not None:
-        st.write("Dados carregados.")
+# Campos para o login
+login = st.text_input("Login")
+senha = st.text_input("Senha", type='password')
 
-    with st.form(key='login_form'):
-        username = st.text_input("Username")
-        password = st.text_input("Password", type='password')
-        submit_button = st.form_submit_button("Login")
-
-        if submit_button:
-            if validate_credentials(username, password, df):
-                st.success("Login bem-sucedido!")
-            else:
-                st.error("Nome de usuário ou senha inválidos.")
-
-if __name__ == "__main__":
-    main()
+if st.button("Entrar"):
+    if verificar_credenciais(login, senha):
+        st.success("Login bem-sucedido!")
+    else:
+        st.error("Login ou senha incorretos.")
